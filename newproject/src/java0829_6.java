ca585b4f0a3c9feef97f8da27c4006eee17c01d9
codest00001 <alldebaro@gmail.com>
2024-08-30 10:53:50 +0900

    0830오전

---
+++ newproject/src/java0829_6.java
@@ -0,0 +1,8 @@
+public class java0829_6 {
+    public static void main(String[] args) {
+        int i, hap = 0;
+        for (i = 1; i <= 10; ++i, hap += i)
+            ;
+        System.out.printf("%d, %d\n", i, hap);
+    }
+}
